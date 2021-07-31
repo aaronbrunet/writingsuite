1ace1ce585b8fb3208b61afc63ceefb8fd913321
aaronbrunet <awbrunet@gmail.com>
2021-07-31 13:36:46 -0400

    updated schema

--- src/graphql/types.tsx
+++ src/graphql/types.tsx
@@ -13,16 +13,34 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
+  GraphQLDateTime: any;
+};
+
+export type Goal = {
+  id: Scalars["ID"];
+  active: Scalars["Boolean"];
+  start: Scalars["GraphQLDateTime"];
+  end: Scalars["GraphQLDateTime"];
+  count: Scalars["Int"];
 };
 
 export type Project = {
   id: Scalars["ID"];
   name: Scalars["String"];
-  description: Scalars["String"];
+  description?: Maybe<Scalars["String"]>;
   active: Scalars["Boolean"];
+  count: Scalars["Int"];
+  goals?: Maybe<Array<Goal>>;
+  wordcounts?: Maybe<Array<WordCount>>;
 };
 
 export type User = {
   id: Scalars["ID"];
   name: Scalars["String"];
+};
+
+export type WordCount = {
+  id: Scalars["ID"];
+  created: Scalars["GraphQLDateTime"];
+  count: Scalars["Int"];
 };
