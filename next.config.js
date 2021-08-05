1ad299b8f969ede1a7f9639d6c0979356e344d31
aaronbrunet <awbrunet@gmail.com>
2021-08-05 18:49:51 -0400

    Added next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.graphql?$/,
+      loader: "webpack-graphql-loader",
+    });
+
+    return config;
+  },
+};
